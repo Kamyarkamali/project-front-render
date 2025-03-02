b9627c24b8488e73f661a236ba71aa5ff077de9c
kamyarkamali <kamyarkamali021@gmail.com>
2025-03-02 21:02:53 +0330

    created blogs  and daynamic routes blogs

--- pages/Academydaynamic.js
+++ pages/Academydaynamic.js
@@ -2,28 +2,32 @@
 import { steps } from "@/data/localData";
 import ShareBtn from "@/module/ShareBtn";
 import { useParams } from "next/navigation";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Academydaynamic() {
-  const { id } = useParams();
+  const params = useParams();
+  const [id, setId] = useState(null);
 
-  // const data = steps.find((item) => item.id.toString() == id);
+  useEffect(() => {
+    if (params?.id) {
+      setId(params.id);
+    }
+  }, [params]);
 
-  // console.log(data);
+  if (!id) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className="flex justify-center">
-      {/* مقدار سمت چپ (شبکه‌های اجتماعی) */}
       <div className="fixed top-[4rem] left-0 w-[100px] h-screen bg-gray-100 p-4">
         Socials
       </div>
 
-      {/* بخش محتوا (بلاگ) */}
       <div className="w-full flex flex-col max-w-[800px] px-8 mt-[4rem]">
-        <div className="h-[1500px]"></div> {/* برای تست ارتفاع */}
+        <div className="h-[1500px]"></div>
       </div>
 
-      {/* مقدار سمت راست (Table of Content - TOC) */}
       <div className="fixed top-[4rem] right-0 w-[200px] h-screen  p-4">
         <ShareBtn />
       </div>
